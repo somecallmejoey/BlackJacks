a6ce209fbce932b100cac14f81432a44fa565cdd
Joey G <121167275+somecallmejoey@users.noreply.github.com>
2023-04-08 13:41:20 +0100

    Adding Javascript
    I have added the ability to get a prompt of the random numebrs that are generated for you sorta like a pack of cards and then enable the display of total of both cards to see how close you are to 21.

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-let firstCard = 3;
-let secondCard = 2;
-let sum = firstCard + secondCard;
+let cards = document.getElementById("cards-el");
+let btn = document.getElementById("btn-start");
+let wrapper = document.getElementsByClassName("wrapper");
+let sums = document.getElementById("sum-el");
 
-let hasBlackJack = true;
-let isAlive = true;
-let message = "";
-
-if (sum < 21) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "You win!";
-  hasBlackJack = true;
-} else {
-  message = "Lol no money for u get fucked";
-  isAlive = false;
+let a = 0;
+let b = 0;
+function startGame(sum) {
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You win!";
+    hasBlackJack = true;
+  } else {
+    message = "Lol no money for u get fucked";
+    isAlive = false;
+  }
+  console.log(message);
 }
 
-// Cash out!
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+function generateTwoCards() {
+  if (cards.textContent !== "") {
+    a = Math.floor(Math.random() * 11) + 1;
+    b = Math.floor(Math.random() * 11) + 1;
+
+    cards.textContent += "Cards: " + a + ", ";
+    cards.textContent += b;
+  }
+}
+
+function addNumbers() {
+  let sum = a + b;
+
+  sums.textContent += "Total =" + " " + sum;
+}
+
+function getRidOfButton() {
+  btn.remove();
+}
