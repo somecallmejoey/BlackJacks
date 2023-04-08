ceb2824442cbec949f6052652c836ddd7e88e4a3
Joey G <121167275+somecallmejoey@users.noreply.github.com>
2023-04-08 11:52:21 +0100

    HasBlackJack
    Defines a variable "hasBlackJack" and if a person got a total of 21, they win.

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ let firstCard = 3;
 let secondCard = 2;
 let sum = firstCard + secondCard;
 
+let hasBlackJack = true;
+
 if (sum < 21) {
   console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
   console.log("You win!");
+  hasBlackJack = true;
 } else {
   console.log("Lol no money for u get fucked");
 }
+
+// Cash out!
+console.log(hasBlackJack);
